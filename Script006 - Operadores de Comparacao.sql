f3950cc034afe298874999bc0629d9f3c16eeb61
Fabricio Chousa <fabrajpa@gmail.com>
2021-10-05 14:30:28 -0300

    Add files via upload

---
+++ Script006 - Operadores de Comparacao.sql
@@ -0,0 +1,16 @@
+/*
+    OPERADORES DE COMPARAÇÃO
+    = (igual a)
+    <> (diferente de)
+    < (menor que)
+    > (maior que)
+    >= (maior ou igual a)
+    <= (menor ou igual a)
+    
+    != (diferente de) [não é padrão ISO]
+    !< (não é menor que) [não é padrão ISO]
+    !> (não é maior que) [não é padrão ISO]
+
+*/
+    SELECT * FROM Person.Person
+     WHERE BusinessEntityID !> 10

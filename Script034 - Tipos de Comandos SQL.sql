c1b42fc6a4066f9e7ea9530f878c04d256816251
Fabricio Chousa <fabrajpa@gmail.com>
2021-10-07 12:48:40 -0300

    Add files via upload

---
+++ Script034 - Tipos de Comandos SQL.sql
@@ -0,0 +1,17 @@
+/*
+    TIPOS DE COMANDOS SQL | TIPOS DE INSTRUÇÕES EM SQL
+
+    * DML - Data Manipulation Language
+        SELECT, UPDATE, DELETE, INSERT, MERGE, BULK INSERT
+            DQL - Data Query Language
+                SELECT
+
+    * DDL - Data Definition Language
+        CREATE, ALTER, DROP, TRUNCATE, DISABLE TRIGGER, ENABLE TRIGGER, UPDATE STATISTICS
+
+    * DCL - Data Control Language
+        GRANT, REVOKE, DENY
+
+    * TCL - Transactional Control Language
+        BEGIN TRANSACTION, COMMIT, ROLLBACK
+*/
